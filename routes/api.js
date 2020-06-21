06f9024b8a9dc09698df3ecddb8641770075aa65
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-06-21 19:38:38 -0400

    Finished HTML routes, cleaned files

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-//Find last workout
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
@@ -12,13 +21,28 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-//updating existing Workout
+router.get("/api/workouts/range", ({ query }, res) => {
+  Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate(
-    params.id,
-    { $push: { exercices: body } },
-    { new: true, runValidators: true }
-  )
+  Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -26,21 +50,5 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
       res.json(err);
     });
 });
-
-//Creating workout
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-//getting Workouts in range
-// router.get("/api/workouts/range", ({ query }, res)=>{
-//     Workout.find({})
-// });
 
 module.exports = router;
